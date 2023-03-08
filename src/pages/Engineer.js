0724f0db5a3929db00b87919f7cd9caa7ca29d54
kouliksarker <koulik.sarker@gmail.com>
2023-03-09 00:57:14 +0530

    Added Login page with signup and sgnin functions

---
+++ src/pages/Engineer.js
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar"
+
+function Engineer() {
+    return (
+        <div className="bg-light">
+            <div className="col-1"><Sidebar home='/' /></div>
+            <div className="container vh-100">
+                <h3 className="text-primary text-center">Welcome{ localStorage.name}</h3>
+                <p className="text-muted text-centre">Take a look at your dashboard</p>
+            </div>
+        </div>
+    )
+}
+
+export default Engineer
